8bd9d4107fe0e04130d539d02cccc0af7984a62c
mdmarjan-pg <mdmarjan313.it@gmail.com>
2026-01-11 22:38:09 +0600

    dom practice

--- DOM TOUR/Js/dom1st.js
+++ DOM TOUR/Js/dom1st.js
@@ -1,5 +1,22 @@
 let liCollection = document.getElementsByTagName("li")
 // console.log(liCollection)
 for(let li of liCollection){
-    console.log(li.innerText)
+    // console.log(li.innerText)
+}
+
+
+let cos1st = document.getElementById('som-titles').style.backgroundColor= 'orange'
+
+
+let sos = document.getElementById('som-titles').innerText
+// console.log(sos)
+
+let tyy = document.getElementById('som-titles')
+tyy.innerText= 'jack ma getting old'
+
+
+
+let places = document.getElementsByClassName('col25')
+for(let opo of places){
+    console.log(opo.innerText)
 }
