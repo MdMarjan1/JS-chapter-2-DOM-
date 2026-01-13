75791c100e2ef857e1bab464cc25aa6ed0a16ed4
mdmarjan-pg <mdmarjan313.it@gmail.com>
2026-01-13 12:36:47 +0600

    dom

--- DOM TOUR/Js/dom1st.js
+++ DOM TOUR/Js/dom1st.js
@@ -1,3 +1,37 @@
-
+// getElementByTagName
 let a1 = document.getElementsByTagName('h1')
-console.log(a1)
+// console.log(a1)
+for(let ar of a1){
+    // console.log(ar.innerText)
+}
+
+let a45 = document.getElementsByTagName('li')
+// console.log(a45)
+for(let aq of a45){
+    // console.log(aq.innerText)
+}
+
+
+let a20 = document.getElementsByClassName('p10')
+for(let az of a20){
+    console.log(az.style.fontSize = '3rem')
+}
+
+let a21 = document.getElementsByClassName('p10')
+for(let auo of a21){
+    console.log(auo.style.color = 'black')
+
+}
+
+let a22 = document.getElementsByClassName('p10')
+for(let apo of a22){
+    console.log(apo.style.backgroundColor = 'green')
+}
+
+let a23 = document.getElementsByClassName('p10')
+for(let apo of a23){
+    console.log(apo.style.textAlign = 'center')
+}
+
+// // getElementByID
+
